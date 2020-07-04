d47cf2d77d719bf1f1ba542e50017540b3aeacfd
Wiut student <cw1.fit.00007510@gmail.com>
2020-07-04 18:29:34 +0500

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt');
 router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Expose-Headers", "x-token" )
 	next();
   });
 
