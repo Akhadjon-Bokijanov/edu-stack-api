efcaafb46eab1c49d8a460608b54ae912109898d
Wiut student <cw1.fit.00007510@gmail.com>
2020-07-06 22:38:01 +0500

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -7,6 +7,7 @@ const { admin, collaborator } = require('../helpers/admin');
 router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-token");
+	res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST, PUT, HEAD");
 	next();
   });
 
