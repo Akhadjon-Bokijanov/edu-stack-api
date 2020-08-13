332808509e1eedf218e302d611459fb5fb15a56a
MLRDer <47412301+MLRDer@users.noreply.github.com>
2020-08-13 11:37:12 -0700

    final test for today

--- routes/news.js
+++ routes/news.js
@@ -19,7 +19,7 @@ const upload = multer({
 
 
 router.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "https://www.edustack.uz"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-token");
 	res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
 	next();
